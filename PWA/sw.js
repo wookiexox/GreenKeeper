70b8a5ed54500982aa8b190453ea00acbaa9110f
wookiexox <lukaszwator26@gmail.com>
2026-05-10 13:44:53 +0200

    cache handling, visual tweaks + resolved adding plant camera button bug

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'greenkeeper-v1';
+const CACHE_NAME = 'greenkeeper-v7';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -7,13 +7,35 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(caches.open(CACHE_NAME).then(cache => {
         return cache.addAll(ASSETS_TO_CACHE);
     }));
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then(response => {
-        return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+    event.waitUntil(caches.keys().then(cacheNames => {
+        return Promise.all(cacheNames.map(cache => {
+            if (cache !== CACHE_NAME) {
+                return caches.delete(cache);
+            }
+        }))
     }));
 });
+
+self.addEventListener('fetch', event => {
+    if (!event.request.url.startsWith(self.location.origin)) return;
+
+    event.respondWith(fetch(event.request).then(response => {
+        if (response && response.status === 200) {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+                cache.put(event.request, responseClone);
+            });
+        }
+        return response;
+        }).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+})
